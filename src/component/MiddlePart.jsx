d44152eb4e027dd962d68f76a44c9e9600ec6d9c
LiuBenny0206 <karta30121@gmail.com>
2024-03-18 16:07:58 +0800

    first edition

--- src/component/MiddlePart.jsx
+++ src/component/MiddlePart.jsx
@@ -13,18 +13,18 @@ function MiddlePart({ data }){
                 <p className="info">
                     {data.location && data.location.city && data.location.region && data.location.postalCode
                     ? `${data.location.city}, ${data.location.region} ${data.location.postalCode}`
-                    : 'Taichung, Taiwan 12345'}
+                    : <p>Taichung, Taiwan <br />12345</p>}
                 </p>
             </div>
             <hr></hr>
             <div className="row">
                 <p className="title">TIMEZONE</p>
-                <p className="info">{data.location ? `UTC ${data.location.timezone}` : 'Loading...'}</p>
+                <p className="info">{data.location && data.location.timezone ? `UTC ${data.location.timezone}` : 'UTC 00:00'}</p>
             </div>
             <hr></hr>
             <div className="row">
                 <p className="title">ISP</p>
-                <p className="info">{data.isp}</p>
+                <p className="info">{data.isp || 'Benny House'}</p>
             </div>
         </div>
     );
