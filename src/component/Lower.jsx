2856b0d001945195690c00720afc39b889a1b698
LiuBenny0206 <karta30121@gmail.com>
2024-03-19 12:51:15 +0800

    second edition

--- src/component/Lower.jsx
+++ src/component/Lower.jsx
@@ -2,33 +2,38 @@ import React, { useEffect } from 'react';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
-function LowerPart({data}) {
+function LowerPart({ data }) {
   useEffect(() => {
-    // Only initialize the map if data.location contains valid coordinates
-    if (data.location.lat && data.location.lng) {
-      var map = L.map('mapPart').setView([data.location.lat, data.location.lng], 13);
-  
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-      }).addTo(map);
+    // Initialize the map with default or actual location
+    var map = L.map('mapPart').setView([
+      (data.location && data.location.lat) || 51.505,
+      (data.location && data.location.lng) || -0.09
+    ], 13);
 
-      var circle = L.circle([data.location.lat, data.location.lng], {
-        color: 'black',
-        fillColor: 'white',
-        fillOpacity: 0.5,
-        radius: 200
-    }).addTo(map);  
-      // Clean-up function
-      return () => {
-        // When the component is unmounted, remove the map
-        map.remove();
-      };
-    }
-  }, [data.location.lat, data.location.lng]); // Add lat and lng as dependencies
-  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+    }).addTo(map);
 
-  return <div id="mapPart"></div>;
+    // Add a circle marker at the location
+    var circle = L.circle([
+      (data.location && data.location.lat) || 51.505,
+      (data.location && data.location.lng) || -0.09
+    ], {
+      color: 'black',
+      fillColor: 'white',
+      fillOpacity: 0.5,
+      radius: 200
+    }).addTo(map);
+
+    // Clean-up function
+    return () => {
+      // When the component is unmounted, remove the map
+      map.remove();
+    };
+  }, [data.location]); // Update the dependency array to just data.location
+
+  return <div id="mapPart" ></div>; // Ensure you set a height for the map container
 }
 
 export default LowerPart;
