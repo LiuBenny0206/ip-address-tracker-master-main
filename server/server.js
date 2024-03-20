16d50fbc79d30225ed100bd23dfe3140fc85d23a
BennyLiu <109209156+LiuBenny0206@users.noreply.github.com>
2024-03-20 11:17:16 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,36 +1,37 @@
 require('dotenv').config();
 
+const express = require('express');
+const app = express();
 const axios = require('axios');
+const cors = require('cors');
 
 const YOUR_API_KEY = process.env.API_KEY;
 
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
+ app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
 
-// 實際的路由處理器
-const handler = async (req, res) => {
-  const ip = req.query.ip;
-  console.log(`Fetching info for IP: ${ip}`);
-  try {
-      // 確保你有一個有效的 API 密鑰，並且正確地存儲和引用它
+  app.get('/get-ip-info', async (req, res) => {
+    const ip = req.query.ip;
+    console.log(`Fetching info for IP: ${ip}`); // 記錄 IP 信息
+    try {
         const response = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Z4jCuBFSlAmVcjb8PGoi2xRO04wrf&ipAddress=${encodeURIComponent(ip)}`);
+        console.log(`Your API key is: ${YOUR_API_KEY}`);
+        console.log('Data received:', response.data); // 記錄收到的數據
       res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: error.message });
-  }
-};
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      res.status(500).json({ error: error.message });
+    }
+  });
 
-module.exports = allowCors(handler);
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
