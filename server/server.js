e0cee0f0a5f447a9005a23d4f85ca127caf9feaa
BennyLiu <109209156+LiuBenny0206@users.noreply.github.com>
2024-03-20 11:00:04 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,38 +1,36 @@
 require('dotenv').config();
 
-const express = require('express');
-const app = express();
 const axios = require('axios');
-const cors = require('cors');
 
 const YOUR_API_KEY = process.env.API_KEY;
 
-  app.get('/get-ip-info', async (req, res) => {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    const ip = req.query.ip;
-    console.log(`Fetching info for IP: ${ip}`); // 記錄 IP 信息
-    try {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+// 實際的路由處理器
+const handler = async (req, res) => {
+  const ip = req.query.ip;
+  console.log(`Fetching info for IP: ${ip}`);
+  try {
+      // 確保你有一個有效的 API 密鑰，並且正確地存儲和引用它
         const response = await axios.get(`https://geo.ipify.org/api/v2/country,city?apiKey=at_Z4jCuBFSlAmVcjb8PGoi2xRO04wrf&ipAddress=${encodeURIComponent(ip)}`);
-        console.log(`Your API key is: ${YOUR_API_KEY}`);
-        console.log('Data received:', response.data); // 記錄收到的數據
       res.json(response.data);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-      res.status(500).json({ error: error.message });
-    }
-  });
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ error: error.message });
+  }
+};
 
-
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
-});
+module.exports = allowCors(handler);
